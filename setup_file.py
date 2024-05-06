3efd0816d6b2ce90a759240da1563e91057a21bb
James <76847057+abreyjames@users.noreply.github.com>
2024-05-06 10:59:35 +0200

    Create setup_file.py
    Add a template file for writing python

---
+++ setup_file.py
@@ -0,0 +1,17 @@
+"""
+File Description
+Author: Guido van Rossum
+"""
+# Imports
+
+# Classes and Functions
+
+# Main function for testing the file
+def main():
+    """
+    Function to test or run your file from
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
